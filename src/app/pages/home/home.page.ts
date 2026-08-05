a2a5f07384779812a1ba96ee4734c288978d56f1
Fredeskrubba <frederikskrubbeltrang@proton.me>
2026-08-05 14:18:46 +0200

    added weekdays overview initial ui

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -10,5 +10,21 @@ import { TopMenuComponent } from 'src/app/components/top-menu/top-menu.component
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, TabsComponent, TopMenuComponent],
 })
 export class HomePage {
+
+ weekdays = Array.from({ length: 7 }, (_, i) => {
+  const date = new Date();
+  const day = date.getDay();
+  const mondayOffset = day === 0 ? -6 : 1 - day;
+
+  date.setDate(date.getDate() + mondayOffset + i);
+
+  return {
+    id: i + 1,
+    name: date.toLocaleDateString("en-US", { weekday: "long" }),
+    date: date.toISOString().split("T")[0],
+    sessionRegistered: true
+  };
+});
+
   constructor() {}
 }
