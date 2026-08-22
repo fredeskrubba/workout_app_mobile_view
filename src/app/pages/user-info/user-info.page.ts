e2fcf146826b02d244a39d7ac0bb228b96ce9904
Fredeskrubba <frederikskrubbeltrang@proton.me>
2026-08-22 09:39:11 +0200

    fixed visual bugs

--- src/app/pages/user-info/user-info.page.ts
+++ src/app/pages/user-info/user-info.page.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
-import { IonHeader, IonToolbar, IonButtons, IonButton, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButtons, IonButton, IonContent, IonIcon, IonFooter } from '@ionic/angular/standalone';
 import { User } from 'src/app/services/user';
 import { GetUserResponse } from 'src/app/models/DTOs/getUserResponse';
 import { Router } from '@angular/router';
-import { arrowBackOutline } from 'ionicons/icons';
+import { arrowBackOutline, logOutOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: 'user-info.page.html',
   styleUrls: ['user-info.page.scss'],
-  imports: [IonIcon, IonHeader, IonToolbar, IonButtons, IonButton, IonContent],
+  imports: [IonIcon, IonHeader, IonToolbar, IonButtons, IonButton, IonContent, IonFooter],
 })
 export class UserinfoPage implements OnInit {
 
@@ -19,7 +19,7 @@ export class UserinfoPage implements OnInit {
   user = signal<GetUserResponse | null>(null);
 
   constructor() {
-    addIcons({ 'back': arrowBackOutline });
+    addIcons({ 'back': arrowBackOutline, "logout": logOutOutline });
   }
 
   ngOnInit(): void {
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { authInterceptor } from './app/interceptors/authInterceptor';
@@ -10,8 +9,10 @@ import { authInterceptor } from './app/interceptors/authInterceptor';
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    provideIonicAngular({
+      animated: false
+    }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 });
